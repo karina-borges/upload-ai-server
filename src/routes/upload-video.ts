1ef5eb393080a7f1d0fd8738a0fed3a68f151d1c
Karina Borges <karina.borges@druid.com.br>
2023-09-12 22:31:52 -0300

    🎉 Basic routes implementation

---
+++ src/routes/upload-video.ts
@@ -0,0 +1,43 @@
+import { fastifyMultipart } from "@fastify/multipart"
+import { FastifyInstance } from "fastify"
+import { randomUUID } from "node:crypto"
+import fs from "node:fs"
+import path from "node:path"
+import { pipeline } from "node:stream"
+import { promisify } from "node:util"
+
+const pump = promisify(pipeline)
+
+export async function uploadVideoRoute(app: FastifyInstance) {
+  app.register(fastifyMultipart, {
+    limits: {
+      fileSize: 1048576 * 25, //25MB
+    },
+  })
+
+  app.post("/videos", async (request, reply) => {
+    const data = await request.file()
+
+    if (!data) {
+      return reply.status(400).send({ error: "No file uploaded" })
+    }
+
+    const extension = path.extname(data.filename)
+
+    if (extension !== ".mp3") {
+      return reply.status(400).send({ error: "Invalid file type" })
+    }
+
+    const fileBaseName = path.basename(data.filename, extension)
+
+    const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`
+
+    const uploadDestination = path.resolve(
+      __dirname,
+      "../../tmp",
+      fileUploadName
+    )
+
+    await pump(data.file, fs.createWriteStream(uploadDestination))
+  })
+}
