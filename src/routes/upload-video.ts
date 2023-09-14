4d5a75b6c7e6ded0dd1d018dc6bacbf3711bcb11
Karina Borges <karina.borges@druid.com.br>
2023-09-14 18:50:15 -0300

    ✨ Integration Open AI

--- src/routes/upload-video.ts
+++ src/routes/upload-video.ts
@@ -5,6 +5,7 @@ import fs from "node:fs"
 import path from "node:path"
 import { pipeline } from "node:stream"
 import { promisify } from "node:util"
+import { prisma } from "../lib/prisma"
 
 const pump = promisify(pipeline)
 
@@ -39,5 +40,14 @@ export async function uploadVideoRoute(app: FastifyInstance) {
     )
 
     await pump(data.file, fs.createWriteStream(uploadDestination))
+
+    const video = await prisma.video.create({
+      data: {
+        name: data.filename,
+        path: uploadDestination,
+      },
+    })
+
+    return { video }
   })
 }
