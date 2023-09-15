1ee8436a381ced23560a40223510f9c41eca4bb2
Karina Borges <karina.borges@druid.com.br>
2023-09-14 22:10:37 -0300

    🚀 Frontend integration

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -1,41 +1,50 @@
-import { FastifyInstance } from "fastify"
-import { z } from "zod"
-import { openai } from "../lib/openai"
-import { prisma } from "../lib/prisma"
+import { OpenAIStream, streamToResponse } from "ai";
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { openai } from "../lib/openai";
+import { prisma } from "../lib/prisma";
 
 export async function generateAICompletionRoute(app: FastifyInstance) {
   app.post("/ai/complete", async (req, reply) => {
     const bodySchema = z.object({
       videoId: z.string().uuid(),
-      template: z.string(),
+      prompt: z.string(),
       temperature: z.number().min(0).max(1).default(0.5),
-    })
+    });
 
-    const { videoId, template, temperature } = bodySchema.parse(req.body)
+    const { videoId, prompt, temperature } = bodySchema.parse(req.body);
 
     const video = await prisma.video.findUniqueOrThrow({
       where: {
         id: videoId,
       },
-    })
+    });
 
     if (!video.transcription) {
       return reply.status(400).send({
         error: "Transcription not found",
-      })
+      });
     }
 
-    const promptMessage = template.replace(
+    const promptMessage = prompt.replace(
       "{transcription}",
       video.transcription
-    )
+    );
 
     const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo-16k",
       temperature,
       messages: [{ role: "user", content: promptMessage }],
-    })
+      stream: true,
+    });
 
-    return response
-  })
+    const stream = OpenAIStream(response);
+
+    return streamToResponse(stream, reply.raw, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST",
+      },
+    });
+  });
 }
